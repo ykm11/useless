f925ae52ca7d0bea1a2916a5938af3d14933a132
ykm11 <ushiromiya3@gmail.com>
2018-09-12 20:58:39 +0900

    _

---
+++ useless/RSA/attack.py
@@ -0,0 +1,20 @@
+from ..math import extgcd
+
+def common_modulus_attack(c1, c2, e1, e2, n):
+
+    g, s, t = extgcd(e1, e2) # s*e1 + t*e2 = g
+    assert s*e1 + t*e2 == g
+
+    print(s, t)
+    if s < 0:
+        s *= -1
+        _, c1, _ = extgcd(c1, n)
+
+    if t < 0:
+        t *= -1
+        _, c2, _ = extgcd(c2, n)
+
+    print(s, t)
+
+    m = pow(c1, s, n) * pow(c2, t, n) % n
+    return m
