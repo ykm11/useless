856460de8712184e6417e41f9ba555382432d7a3
ykm11 <ushiromiya3@gmail.com>
2018-12-01 16:28:28 +0900

    update utils.py

--- useless/utils.py
+++ useless/utils.py
@@ -8,6 +8,19 @@ def le_4_num2bytes(x):
 def le_4_bytes2num(data):
     return struct.unpack("<I", data)[0]
 
+def be_4_num2bytes(x):
+    return struct.pack(">I", x)
+
+def be_4_bytes2num(data):
+    return struct.unpack(">I", data)[0]
+
+def le_8_num2bytes(x):
+    return struct.pack("<Q", x)
+
+def le_8_bytes2num(data):
+    return struct.unpack("<Q", data)[0]
+
+
 def le_num2bytes(x):
     hex_x = hex(x)[2:]
     if len(hex_x) % 2 != 0:
@@ -16,3 +29,14 @@ def le_num2bytes(x):
 
 def le_bytes2num(data):
     return int(binascii.hexlify(data[::-1]), 16)
+
+def num2bytes(x):
+    hex_x = hex(x)[2:]
+    if len(hex_x) % 2 != 0:
+        hex_x = '0' + hex_x
+    return binascii.unhexlify(hex_x)
+
+def bytes2num(data):
+    return int(binascii.hexlify(data), 16)
+
+
