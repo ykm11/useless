717c101f88fde2a7c6898b7cc16edb7c5efd95d5
ykm11 <ushiromiya3@gmail.com>
2019-03-02 21:13:05 +0900

    _

--- useless/crypto/math.py
+++ useless/crypto/math.py
@@ -52,7 +52,7 @@ def CRT(a1, a2, m1, m2):
 def modinv(x, m):
     g, s, t = extgcd(x, m)
     if g != 1:
-        raise ValueError("inverse element does not exist over m")
+        raise ValueError("inverse element does not exist over Z({0})".format(m))
     return s % m
 
 def xor(x, y):
